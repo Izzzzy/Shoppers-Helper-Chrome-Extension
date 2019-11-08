8e6e74b5788736ee1f399a1ebf997baeb5ad1c5c
Yitzi Yelen <izzy@bitbean.com>
2019-11-08 09:47:21 -0500

    removed going to all categories on eBay rather than to the home page

--- universal.js
+++ universal.js
@@ -8,4 +8,4 @@
 
 // var ebayLink=currentPage.indexOf('ebay.com') > -1?'http://rover.ebay.com/rover/1/711-53200-19255-0/1?ff3=4&pub=5575226091&toolid=10001&campid=5337959871&customid=&mpre=http%3A%2F%2Fwww.ebay.com%2Fsch%2Fi.html%3F_nkw%3D':'http://ebay.com%2Fsch%2Fi.html%3F_nkw%3D';
 
-var ebayLink='http://rover.ebay.com/rover/1/711-53200-19255-0/1?ff3=4&pub=5575226091&toolid=10001&campid=5337959871&customid=&mpre=http%3A%2F%2Fwww.ebay.com%2Fsch%2Fi.html%3F_nkw%3D';
+var ebayLink='http://rover.ebay.com/rover/1/711-53200-19255-0/1?ff3=4&pub=5575226091&toolid=10001&campid=5337959871&customid=&mpre=http%3A%2F%2Fwww.ebay.com';
